9bd6690229881b4a0e00b15521bb6ba70421142a
alejandro <alejandro.martin@uam.es>
2017-12-21 12:24:20 +0100

    fixing emulator64

---
+++ executeStraceIn.sh
@@ -0,0 +1,1 @@
+strace -p 36 -f -ttF -o /sdcard/0a0f25f2e9f48207189af2b77f3b511b01c3bd0ecd9266e831e3bbc1a302c6da.apk_strace_monkey.txt
--- startemuNoGUI.sh
+++ startemuNoGUI.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-emulator -avd $1 -system images/system.img -ramdisk images/ramdisk.img -wipe-data -prop dalvik.vm.execution-mode=int:portable -no-audio -no-window -memory 2000 &
+emulator64 -avd $1 -system images/system.img -ramdisk images/ramdisk.img -wipe-data -prop dalvik.vm.execution-mode=int:portable -no-audio -no-window -memory 2000 &
