1c5ec75a9cc061003a397867a38e5a5f6d3411e5
alejandro <alejandro.martin@uam.es>
2017-12-21 13:46:46 +0100

    fixing emulator64

--- startemuNoGUI.sh
+++ startemuNoGUI.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-emulator64 -avd $1 -system images/system.img -ramdisk images/ramdisk.img -wipe-data -prop dalvik.vm.execution-mode=int:portable -no-audio -no-window -memory 2000 &
+emulator64-arm -avd $1 -system images/system.img -ramdisk images/ramdisk.img -wipe-data -prop dalvik.vm.execution-mode=int:portable -no-audio -no-window -memory 2000 &
